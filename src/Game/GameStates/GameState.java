94b235a2815f8a93104bfa1ffd0cf221de99d008
marco <marcos.camacho@upr.edu>
2018-10-25 11:07:18 -0400

    Working on implementing a 3rd world.

--- src/Game/GameStates/GameState.java
+++ src/Game/GameStates/GameState.java
@@ -16,7 +16,8 @@ public class GameState extends State {
         super(handler);
         Player player = new Player(handler, 100, 100);
         BaseWorld world = new World1(handler, "res/Maps/map1.map", player);
-        handler.setWorld(world);
+        BaseWorld world2 = new World1(handler, "res/Maps/fieldMap.map", player);
+        handler.setWorld(world2);
         handler.getWorld().getEntityManager().setPlayer(player);
     }
 
